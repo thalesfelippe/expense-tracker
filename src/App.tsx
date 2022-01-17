2c89fbc6412f110ace4dbb57c0fe67793ed1d875
Thales Felippe <thales_so@hotmail.com>
2022-01-17 14:14:21 -0300

    Alternancia entre um mes e outro

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { categories } from "./data/categories";
 import { items } from "./data/items";
 import { getCurretMonth, filterListByMonth } from "./helpers/dateFilter";
 import { TableArea } from "./components/TableArea";
+import { InfoArea } from "./components/InfoArea";
 
 const App = () => {
   const [list, setlist] = useState(items);
@@ -16,6 +17,10 @@ const App = () => {
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
 
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -24,7 +29,11 @@ const App = () => {
       <C.Body>
         
         {/* Área de informações */}
-
+        <InfoArea
+         currentMonth={currentMonth}
+         onMonthChange={handleMonthChange}
+         />
+          
         {/* Área de inserção */}
 
         {/* Tabela de items */}
