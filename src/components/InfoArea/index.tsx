e77507a8911faaf6cd1a61a20db255648e0863c4
Thales Felippe <thales_so@hotmail.com>
2022-01-18 14:22:55 -0300

    info area

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,12 +1,15 @@
 import * as C from "./styles";
 import { formatCurrentMonth } from "../../helpers/dateFilter";
+import { ResumeItem } from "../ResumeItem";
 
 type Props = {
     currentMonth: string;
     onMonthChange: (newMonth: string) => void;
+    income: number;
+    expense: number;
 }
 
-export const InfoArea = ({ currentMonth, onMonthChange }: Props) => {
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
     const handlePrevMonth = () => {
         let [year, month] = currentMonth.split('-');
         let currentDate = new Date(parseInt(year), parseInt(month) -1, 1);
@@ -31,7 +34,9 @@ export const InfoArea = ({ currentMonth, onMonthChange }: Props) => {
             </C.MonthArea>
 
             <C.ResumeArea>
-
+                <ResumeItem title="Receitas" value={income}/>
+                <ResumeItem title="Despesas" value={expense}/>
+                <ResumeItem title="Balanço" value={income - expense} color={(income-expense) < 0 ? 'red' : 'green'}/>
             </C.ResumeArea>
         </C.Container>
 
